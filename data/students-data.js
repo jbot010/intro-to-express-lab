b2f5b91c573ac0fc56b5eee0f10d7f89c6151c0d
Julian <julian.franco@icloud.com>
2023-04-11 20:24:00 -0400

    import students inside server.js

---
+++ data/students-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {text: 'Leeloo Dallas', done: true, _id: 125223},
+  {text: 'Dominic Toretto', done: false, _id: 127904},
+  {text: 'Steve Rogers', done: false, _id: 139608},
+]
+
+export { 
+	students
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+// import modules
+
+import express from 'express'
+import { students } from './data/students-data.js'
+
+// create Express app
+
+const app = express()
+
+// configure the app (app.set)
+app.set('view engine', 'ejs')
+
+
+// mount Middleware (app.use)
+
+
+
+// mount routes
+app.get('/home', function(req, res) {
+  res.render('home')
+})
+
+
+// tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
